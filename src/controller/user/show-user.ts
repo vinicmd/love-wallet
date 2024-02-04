5c3e22426ea6fc71111f39685f47b0a916aa44a5
Vinicius Duarte <viniciusduarte@outlook.com>
2024-02-04 15:16:46 +0000

    refactor in show user validations

--- src/controller/user/show-user.ts
+++ src/controller/user/show-user.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
-import { badRequest } from '../../helper/http'
+import { badRequest, notFound } from '../../helper/http'
 import { MissingParamError } from '../../errors/missing-param-error'
 import { dbGetUser } from '../../db/usecase/user/get-user'
 import { InvalidParamError } from '../../errors/invalid-param-error'
@@ -17,6 +17,10 @@ export const showUser = async (req: Request, res: Response) => {
     }
 
     const user = await dbGetUser(userId)
+
+    if (!user) {
+      return notFound(res)
+    }
 
     return res.status(200).json(user)
   } catch (error) {
--- src/helper/http.ts
+++ src/helper/http.ts
@@ -4,3 +4,7 @@ export const badRequest = (response: Response, error: string) => {
   console.error(error)
   return response.sendStatus(400)
 }
+
+export const notFound = (response: Response) => {
+  return response.sendStatus(404)
+}
